72d31f8029ec2e632a5296c931e826d4be575922
Samreen <sammahak.20@gmail.com>
2016-12-03 19:21:51 +0200

    soundoff done

---
+++ client_test.py
@@ -0,0 +1,25 @@
+# Setup Python logging --------------------------------------------------------
+import logging
+FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
+logging.basicConfig(level=logging.DEBUG,format=FORMAT)
+LOG = logging.getLogger()
+
+import paho.mqtt.client as mqtt
+import protocol.client.client_protocol as cp
+from protocol.common import *
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/".join((DEFAULT_ROOT_TOPIC, GLOBAL, SELF)))
+    client.publish("/".join((DEFAULT_ROOT_TOPIC, GLOBAL)), " ".join((SOUND_OFF, SELF)))
+
+nickname = "DATNAME"
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = lambda client, userdata, msg: cp.message_in(None, client, userdata, msg)
+client.connect(DEFAULT_SERVER_URL, DEFAULT_SERVER_PORT)
+
+client.loop_forever()
--- protocol/client/client_protocol.py
+++ protocol/client/client_protocol.py
@@ -4,20 +4,19 @@ FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 logging.basicConfig(level=logging.DEBUG,format=FORMAT)
 LOG = logging.getLogger()
 
-from common import *
-import protocol.server.global_functions as wf
-import protocol.server.server_functions as sf
+from protocol.common import *
+import protocol.client.global_functions as wf
+import protocol.client.server_functions as sf
 
-def message_in(server_obj, client, userdata, msg):
+def message_in(client_obj, mqttclient, userdata, msg):
     if not msg.topic.startswith(DEFAULT_ROOT_TOPIC + "/"):
         return
     topic = msg.topic[DEFAULT_ROOT_LEN:].split("/")
     if topic[0] in CATEGORIES:
         LOG.info("Received message from %s topic"%CATEGORIES[topic[0]])
         if topic[0] == GLOBAL:
-            wf.message_in(server_obj, client, topic[1:], msg.payload.split(" "))
+            wf.message_in(client_obj, mqttclient, topic[1:], msg.payload.split(" "))
         elif topic[0] == SERVER:
-            sf.message_in(server_obj, client, topic[1:], msg.payload.split(" "))
+            sf.message_in(client_obj, mqttclient, topic[1:], msg.payload.split(" "))
 
 
-#def sound_off(client):
